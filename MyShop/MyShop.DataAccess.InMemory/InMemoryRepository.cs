9a38f0d3f67caf83a63c62a05812bd3960aa0821
ahmed yaqot <ahmedyaqot95@outlook.com>
2020-06-25 02:13:22 +0300

    Dependency injection and microsoft unity

--- MyShop/MyShop.DataAccess.InMemory/InMemoryRepository.cs
+++ MyShop/MyShop.DataAccess.InMemory/InMemoryRepository.cs
@@ -1,4 +1,5 @@
-﻿using MyShop.Core.Models;
+﻿using MyShop.Core.Contracts;
+using MyShop.Core.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,18 +9,18 @@ using System.Threading.Tasks;
 
 namespace MyShop.DataAccess.InMemory
 {
-    public class InMemoryRepository<T> where T : BaseEntity 
+    public class InMemoryRepository<T> : IRepository<T> where T : BaseEntity
     {
         ObjectCache cache = MemoryCache.Default;
         List<T> items;
         string className;
-        
+
         public InMemoryRepository()
         {
             className = typeof(T).Name;
             items = cache[className] as List<T>;
             if (items == null)
-                items = new List<T>() ;
+                items = new List<T>();
         }
 
         public void Commit()
